c28c34983c668552cbbdb57d58a93535c09c6ebf
Markobriga <marko.brigic@gmail.com>
2022-01-11 17:57:11 +0100

    Napravljen osnovni model headera

--- components/navbar.js
+++ components/navbar.js
@@ -6,6 +6,7 @@ const NavBar = () => {
             {navigationItems.map((item) => (
                 <li
                     key={item}
+                    className="px-4 py-2 border"
                 >
                     {item}
                 </li>
--- modules/header/header.js
+++ modules/header/header.js
@@ -1,21 +1,29 @@
 import NavBar from "../../components/navbar";
 import Image from 'next/image';
+import LogoImg from '../../assets/logo.png';
 
 const Header = () => {
     return (
-        <header className="flex items-center">
-            <main>
-                <div>
+        <header className="flex ">
+            <main className="inline-flex"> {/* Zašto ne radi flex-row */}
+                <div >
+                    <Image
+                        src={LogoImg}
+                        layout="fixed"
+                        width={100}
+                        height={100}
+                    />
+                </div>
+                <div >
                     <h1 >
                         MEDUZA
                     </h1>
                     <h3>
                         Rent a Boat Company
                     </h3>
+                    <NavBar /> {/*Kako izdvojit Sign in */}
                 </div>
-                <div>
-                    <NavBar />
-                </div>
+
             </main>
 
         </header>
