495694ebf738872c1e8d8070ac2347b5146b2fb2
Markobriga <marko.brigic@gmail.com>
2022-02-09 16:27:03 +0100

    Riješen bug na Vercelu

--- const/scheduleList.js
+++ const/scheduleList.js
@@ -8,42 +8,42 @@ export const scheduleList = [
     {
         time: "7:10",
         text: "Quicksilver 755 parts off.",
-        Icon_Src: "/images/Sea-icons/sea-green.png"
+        Icon_Src: "/images/sea-icons/sea-green.png"
     },
 
-    {   
+    {
         time: "7:10",
         text: "Quicksilver 555 parts off.",
-        Icon_Src: "/images/Sea-icons/sea-lightBlue.png"
+        Icon_Src: "/images/sea-icons/sea-lightBlue.png"
     },
-    
+
     {
         time: "7:50",
         text: "Quicksilver 777 parts off.",
-        Icon_Src: "/images/Sea-icons/sea-red.png"
+        Icon_Src: "/images/sea-icons/sea-red.png"
     },
 
-   /*  {
-        time: "14:30",
-        text: "Švecki sajam u Biogradu.",
-        Icon_Src: ""
-    }, */
+    /*  {
+         time: "14:30",
+         text: "Švecki sajam u Biogradu.",
+         Icon_Src: ""
+     }, */
 
     {
         time: "17:30",
         text: "Quicksilver 755 arrives",
-        Icon_Src: "/images/Boat-icons/boat-green.png"
+        Icon_Src: "/images/boat-icons/boat-green.png"
     },
 
     {
         time: "17:45",
         text: "Barracuda 755 arrives",
-        Icon_Src: "/images/Boat-icons/boat-darkBlue.png"
+        Icon_Src: "/images/boat-icons/boat-darkBlue.png"
     },
 
     {
         time: "18:00",
         text: "Quicksilver 555 arrives",
-        Icon_Src: "/images/Boat-icons/boat-lightBlue.png"
+        Icon_Src: "/images/boat-icons/boat-lightBlue.png"
     }
 ]
