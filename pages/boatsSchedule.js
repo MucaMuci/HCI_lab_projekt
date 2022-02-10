3b8b385fe5de5d10c01b8f2316e05f457922dc69
Markobriga <marko.brigic@gmail.com>
2022-02-10 13:55:07 +0100

    Popravljen boats schedule

--- pages/boatsSchedule.js
+++ pages/boatsSchedule.js
@@ -43,27 +43,27 @@ const BoatsSchedule = () => {
                     <div className="border border-hci-siva shadow-btn-sjena rounded-3xl sm:w-2/5 mb-5 h-fit mr-3">
                         <Calendar className="text-center" onChange={onChange} value={date} />
                     </div>
-                    <div className="flex flex-col border border-hci-siva shadow-btn-sjena rounded-3xl grow ">
+                    <div className="flex flex-col border font-medium border-hci-siva shadow-btn-sjena rounded-3xl grow ">
                         <div className="bg-hci-siva-2 flex justify-center border-t border-hci-siva py-2 rounded-t-3xl">
-                            <div className="">
+                            <div className="text-2xl">
                                 WEATHER
                             </div>
                         </div>
-                        <div className="flex justify-between px-3 py-2">
-                            <div className="flex">
-                                <div className="pr-2">
+                        <div className="flex px-3 py-2 text-lg">
+                            <div className="flex flex-1">
+                                <div className="pr-2 ">
                                     Temperature:
                                 </div>
                                 <div>
                                     33°C
                                 </div>
                             </div>
-                            <div className="pt-1">
+                            <div className="pt-1 flex-1 text-center ">
                                 <Image src={SunImg}
                                     width={20}
                                     height={20} />
                             </div>
-                            <div className="flex">
+                            <div className="flex flex-1 justify-end">
                                 <div className="pr-2">
                                     Wind:
                                 </div>
@@ -74,32 +74,32 @@ const BoatsSchedule = () => {
 
                         </div>
                         <div className="bg-hci-siva-2 flex justify-center py-2 border-t border-hci-siva">
-                            <div className="">
+                            <div className="text-2xl">
                                 FREE BOAT&apos;S
                             </div>
                         </div>
                         <div className="flex flex-col pb-5 px-2">
-                            <div className="table text-center table-auto">
-                                <div className="table-header-group">
-                                    <div className="table-row">
-                                        <div className="table-cell text-left">BOAT&apos;S NAME</div>
-                                        <div className="table-cell ">PRICE</div>
-                                        <div className="table-cell ">MAX. PASSANGERS</div>
-                                        <div className="table-cell ">LENGTH</div>
+                            <div className="w-full text-center font-medium">
+                                <div className="font-medium text-sm">
+                                    <div className="flex">
+                                        <div className="text-left w-1/3 px-1">BOAT&apos;S NAME</div>
+                                        <div className=" w-1/6">PRICE</div>
+                                        <div className=" w-1/3">MAX. PASSANGERS</div>
+                                        <div className=" w-1/6">LENGTH</div>
                                     </div>
                                 </div>
-                                <div className=" table-row-group ">
-                                    <div className="table-row bg-green-300 ">
-                                        <div className="table-cell text-left">Barracudda 734</div>
-                                        <div className="table-cell">400€</div>
-                                        <div className="table-cell">7</div>
-                                        <div className="table-cell">7.34m</div>
+                                <div className=" ">
+                                    <div className="flex bg-green-200 mb-2 rounded-md px-1">
+                                        <div className="text-left w-1/3 ">Barracudda 734</div>
+                                        <div className="w-1/6">400€</div>
+                                        <div className="w-1/3 ">7</div>
+                                        <div className="w-1/6">7.34m</div>
                                     </div>
-                                    <div className="table-row bg-green-300  ">
-                                        <div className="table-cell text-left">Bigbarka33 </div>
-                                        <div className="table-cell">400€</div>
-                                        <div className="table-cell">7</div>
-                                        <div className="table-cell">7.34m</div>
+                                    <div className="flex bg-green-200 rounded-md px-1">
+                                        <div className="text-left w-1/3">Bigbarka33 </div>
+                                        <div className="w-1/6">400€</div>
+                                        <div className="w-1/3">7</div>
+                                        <div className="w-1/6">7.34m</div>
                                     </div>
 
                                 </div>
@@ -107,44 +107,44 @@ const BoatsSchedule = () => {
                         </div>
 
                         <div className="bg-hci-siva-2 flex justify-center py-2 border-t border-hci-siva">
-                            <div className="">
+                            <div className="text-2xl">
                                 RESERVED BOAT&apos;S
                             </div>
                         </div>
-                        <div className="rounded-b-3xl pb-5 flex flex-col px-2">
-                            <div className="table text-center">
-                                <div className="table-header-group">
-                                    <div className="table-row">
-                                        <div className="table-cell text-left">BOAT&apos;S NAME</div>
-                                        <div className="table-cell text-left">PRICE</div>
-                                        <div className="table-cell ">PASSANGERS</div>
-                                        <div className="table-cell ">LENGTH</div>
+                        <div className="flex flex-col pb-5 px-2">
+                            <div className="w-full text-center font-medium">
+                                <div className="font-medium text-sm">
+                                    <div className="flex">
+                                        <div className="text-left w-1/3 px-1">BOAT&apos;S NAME</div>
+                                        <div className=" w-1/6">PRICE</div>
+                                        <div className=" w-1/3">PASSANGERS</div>
+                                        <div className=" w-1/6">LENGTH</div>
                                     </div>
                                 </div>
-                                <div className=" table-row-group ">
-                                    <div className="table-row bg-red-300 ">
-                                        <div className="table-cell text-left">Barracudda 755</div>
-                                        <div className="table-cell text-left">360€</div>
-                                        <div className="table-cell">5/7</div>
-                                        <div className="table-cell">6.55m</div>
+                                <div className=" ">
+                                    <div className="flex bg-red-200 mb-2 rounded-md px-1">
+                                        <div className="text-left w-1/3 ">Barracudda 755</div>
+                                        <div className="w-1/6">360€</div>
+                                        <div className="w-1/3 ">5/7</div>
+                                        <div className="w-1/6">6.55m</div>
                                     </div>
-                                    <div className="table-row bg-red-300  ">
-                                        <div className="table-cell text-left">Quicksilver 555</div>
-                                        <div className="table-cell text-left">400€</div>
-                                        <div className="table-cell">3/6</div>
-                                        <div className="table-cell">5.78m</div>
+                                    <div className="flex bg-red-200 rounded-md px-1 mb-2">
+                                        <div className="text-left w-1/3">Quicksilver 555 </div>
+                                        <div className="w-1/6">400€</div>
+                                        <div className="w-1/3">3/6</div>
+                                        <div className="w-1/6">5.78m</div>
                                     </div>
-                                    <div className="table-row bg-red-300  ">
-                                        <div className="table-cell text-left">Mala Barka 333</div>
-                                        <div className="table-cell text-left">90€</div>
-                                        <div className="table-cell">5/5</div>
-                                        <div className="table-cell">4.75m</div>
+                                    <div className="flex bg-red-200 rounded-md px-1 mb-2">
+                                        <div className="text-left w-1/3">Mala Barka 333 </div>
+                                        <div className="w-1/6">90€</div>
+                                        <div className="w-1/3">5/5</div>
+                                        <div className="w-1/6">4.75m</div>
                                     </div>
-                                    <div className="table-row bg-red-300  ">
-                                        <div className="table-cell text-left">Domaći Kaić 82</div>
-                                        <div className="table-cell text-left">100€</div>
-                                        <div className="table-cell">4/6</div>
-                                        <div className="table-cell">5.00m</div>
+                                    <div className="flex bg-red-200 rounded-md px-1 mb-2">
+                                        <div className="text-left w-1/3">Domaći Kaić 82 </div>
+                                        <div className="w-1/6">100€</div>
+                                        <div className="w-1/3">4/6</div>
+                                        <div className="w-1/6">5.00m</div>
                                     </div>
 
                                 </div>
