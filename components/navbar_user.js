99a0cb9748f50329013974f375a495aa15803fa8
Markobriga <marko.brigic@gmail.com>
2022-02-11 15:05:57 +0100

    Sitne popravke

--- components/navbar_user.js
+++ components/navbar_user.js
@@ -9,10 +9,10 @@ const NavBarUser = () => {
     return (
         <nav className="flex list-none bg-hci-header">
             {navigationItems.map((link, index) => (
-                <ul className={router.pathname == link.path ? "bg-white text-hci-modra px-1 mx-1 my-2 rounded-3xl hover:cursor-pointer" : "text-white mx-2 my-2 hover:cursor-pointer "}>
+                <ul className={router.pathname == link.path ? "bg-white text-hci-modra px-1 mx-1 my-2 rounded-3xl hover:cursor-pointer" : "text-white mx-2 my-2 hover:cursor-pointer "} key={index}>
                     <Link href={link.path}>
                         <li
-                            key={index}
+
                             className="  self-end text-lg"
                         >
                             {link.name}
