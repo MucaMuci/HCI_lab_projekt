35e47e1f1737f28559abde0d97637f57a104c6a3
Markobriga <marko.brigic@gmail.com>
2022-02-14 17:34:16 +0100

    Dodani nazivi koraka

--- components/formCard.js
+++ components/formCard.js
@@ -6,20 +6,22 @@ import EmptyGreyCircle from "../assets/empty_circle_grey.png"
 
 const FormCard = ({ children, currentStep, prevFormStep }) => {
     return (
-        <div className="flex flex-col max-w-6xl mx-auto pb-10">
+        <div className="flex flex-col max-w-5xl mx-auto pb-10">
             {currentStep < 3 &&
                 <div className="flex items-center justify-center">
-                    <div>
-                        {currentStep < 1 ?
-                            <Image src={EmptyCircle}
-                                width={40}
-                                height={40}
-                                alt="Prazni krug" /> :
-                            <Image src={FullCircle}
-                                width={40}
-                                height={40}
-                                alt="Puni krug" />
-                        }
+                    <div className="flex flex-col items-end">
+                        <div className="">
+                            {currentStep < 1 ?
+                                <Image src={EmptyCircle}
+                                    width={40}
+                                    height={40}
+                                    alt="Prazni krug" /> :
+                                <Image src={FullCircle}
+                                    width={40}
+                                    height={40}
+                                    alt="Puni krug" />
+                            }</div>
+
                     </div>
                     {currentStep < 1 ?
                         <div className='self-center mb-1 border border-solid w-1/4 h-0 border-hci-siva'></div>
@@ -27,23 +29,26 @@ const FormCard = ({ children, currentStep, prevFormStep }) => {
                     }
 
                     <div>
-                        {currentStep == 0 &&
-                            <Image src={EmptyGreyCircle}
-                                width={40}
-                                height={40}
-                                alt="Prazni sivi krug" />}
-                        {currentStep == 1 &&
-                            <Image src={EmptyCircle}
-                                width={40}
-                                height={40}
-                                alt="Prazni krug" />
-                        }
-                        {currentStep == 2 &&
-                            <Image src={FullCircle}
-                                width={40}
-                                height={40}
-                                alt="Prazni krug" />
-                        }
+                        <div>
+                            {currentStep == 0 &&
+                                <Image src={EmptyGreyCircle}
+                                    width={40}
+                                    height={40}
+                                    alt="Prazni sivi krug" />}
+                            {currentStep == 1 &&
+                                <Image src={EmptyCircle}
+                                    width={40}
+                                    height={40}
+                                    alt="Prazni krug" />
+                            }
+                            {currentStep == 2 &&
+                                <Image src={FullCircle}
+                                    width={40}
+                                    height={40}
+                                    alt="Prazni krug" />
+                            }
+                        </div>
+
                     </div>
                     {currentStep < 2 ?
                         <div className='self-center mb-1 border border-solid w-1/4 h-0 border-hci-siva'></div>
@@ -65,6 +70,19 @@ const FormCard = ({ children, currentStep, prevFormStep }) => {
 
                 </div>
             }
+
+            <div className="hidden sm:flex max-w-5xl mx-auto w-11/12 ">
+                <div className="flex-1 text-center">
+                    Reservation details
+                </div>
+                <div className="flex-1 text-center">
+                    Customers information
+                </div>
+                <div className="flex-1  pr-2 text-center">
+                    Payment
+                </div>
+            </div>
+
             <div className="max-w-5xl mx-auto ">
                 {children}
             </div>
