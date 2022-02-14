96e267b6b8c49be4d89f94856914fb68285306d6
Markobriga <marko.brigic@gmail.com>
2022-02-14 17:20:13 +0100

    Dodan gumb

--- pages/listofboats.js
+++ pages/listofboats.js
@@ -5,6 +5,8 @@ import Filter from "../modules/filter/filter";
 import Footer from "../modules/footer/footer";
 import Header from "../modules/header/header";
 import SEO from '../data/next-seo.config';
+import WhitePlus from "../assets/white_plus.png";
+import Image from "next/image";
 
 const ListOfBoats = () => {
     return (
@@ -25,6 +27,22 @@ const ListOfBoats = () => {
                 </div>
                 <div className="flex max-w-5xl mx-auto justify-center">
                     <Boats />
+                </div>
+                <div className="max-w-5xl mx-auto flex justify-center">
+
+                    <button className='border shadow-btn-sjena rounded-lg border-hci-modra bg-hci-modra items-center w-fit px-3 my-6  flex '>
+                        <div className='flex text-white justify-center pr-2'>
+                            Add another boat
+                        </div>
+                        <div className='pt-1'>
+                            <Image
+                                src={WhitePlus}
+                                width={18}
+                                height={18}
+                                alt="WhitePlus"
+                            />
+                        </div>
+                    </button>
                 </div>
             </div>
             <div className='absolute w-full bottom-0'>
