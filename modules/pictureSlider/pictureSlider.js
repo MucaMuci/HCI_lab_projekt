cfb7d1b0f5984e1bdaa898308d4f25e13fee261e
MucaMuci <matejj.mucic@gmail.com>
2022-02-24 11:36:16 +0100

    napravljen slider fale slike

--- modules/pictureSlider/pictureSlider.js
+++ modules/pictureSlider/pictureSlider.js
@@ -1,77 +1,37 @@
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import "swiper/css/navigation";
+import { Navigation, Scrollbar, Virtual } from 'swiper';
+
 import Image from "next/image";
-import { useEffect } from "react";
+
 import LeftArrowImage from "../../assets/left_arrow.png"
 import RightArrowImage from "../../assets/right_arrow.png"
 
-function LeftArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-        <div
-            className={className}
-            style={{ ...style, display: "block", background: "green", height: "30px", width: "30px" }}
-            onClick={onClick}
-        />
-        /* <Image
-            src={LeftArrowImage}
-        /> */
-    );
-}
-function RightArrow(props) {
-    const { className, style, onClick } = props;
-    return (
-        <div
-            className={className}
-            style={{ ...style, display: "block", background: "green", height: "30px", width: "30px" }}
-            onClick={onClick}
-        />
-        /*  <Image
-             className={className}
-             src={RightArrowImage}
-             onClick={onClick}
-         /> */
-    );
-}
 const PictureSlider = ({ listOfPictures }) => {
 
-    const sliderSettings = {
-        dots: true,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        rows: 1,
-        slidesPerRow: 1,
-        nextArrow: <RightArrow />,
-        prevArrow: <LeftArrow />
-    }
-
     return (
-        <div>
-            <Slider {...sliderSettings}>
-                <div className="bg-red-400">
-                    5
-                </div>
-                <div className="bg-green-400">
-                    6
-                </div>
-
-                {/* {
-                    listOfPictures.map((e, index) => (
-                        <div className="flex w-auto" key={index}>
-                            <Image
-                                src={e}
-                                height={40}
-                                width={40}
-                            />
-                        </div>
-                    ))
-                } */}
-
-            </Slider>
-        </div>
+        <Swiper className='w-full'
+            navigation={true}
+            modules={[Navigation]}
+            spaceBetween={50}
+            slidesPerView={1}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+        >
+            {
+                listOfPictures.map((e, index) => (
+                    <SwiperSlide className="flex w-auto justify-center" key={index}>
+                        <Image
+                            src={e}
+                            height={40}
+                            width={40}
+                            alt="slika"
+                        />
+                    </SwiperSlide>
+                ))
+            }
+        </Swiper>
     );
 }
 
