7a3f5636a790af933602603212c268b440304915
MucaMuci <matejj.mucic@gmail.com>
2022-02-28 20:54:55 +0100

    popravljeno

--- modules/boats/boats2.js
+++ modules/boats/boats2.js
@@ -1,52 +1,56 @@
-import Boat from "../../components/boatUser"
+import Boat from "../../components/boatUser";
 //import { boats } from "../../const/boatList"
 
-import { db } from '../../firebase/initFirebase'
-import { doc, getDocs, collection } from "firebase/firestore"
-import { useUser } from '../../firebase/useUser'
-import { async } from "@firebase/util"
-import { useEffect, useRef, useState } from "react"
+import { db } from "../../firebase/initFirebase";
+import { doc, getDocs, collection } from "firebase/firestore";
+import { useUser } from "../../firebase/useUser";
+import { async } from "@firebase/util";
+import { useEffect, useRef, useState } from "react";
 
 const Boats = ({ MPFilter, NSFilter, BLFilter, BHPFilter }) => {
+  const [boats, setBoats] = useState([]);
+  const [copy, setCopy] = useState([]);
 
-    const [boats, setBoats] = useState([]);
-    const [copy, setCopy] = useState([]);
+  //boat.id.toLowerCase().contains(NSFilter.toLowerCase())
 
-    //boat.id.toLowerCase().contains(NSFilter.toLowerCase())
+  useEffect(() => {
+    const ListOfBoats = [];
+    getDocs(collection(db, "Boats")).then((snapshot) => {
+      snapshot.docs.forEach((doc) => {
+        // boats.push({ ...doc.data(), id: doc.id })
+        ListOfBoats.push({ ...doc.data(), id: doc.id });
+      });
+      setBoats(ListOfBoats);
+      setCopy(ListOfBoats);
+    });
+  }, []);
 
-    useEffect(() => {
-        const ListOfBoats = [];
-        getDocs(collection(db, "Boats")).then((snapshot) => {
-            snapshot.docs.forEach(doc => {
-                // boats.push({ ...doc.data(), id: doc.id })
-                ListOfBoats.push({ ...doc.data(), id: doc.id })
-            })
-            setBoats(ListOfBoats);
-            setCopy(ListOfBoats);
-        })
-    }, [])
+  useEffect(() => {
+    const passengerFilter = (boat) => boat.MaxPassengers >= MPFilter;
+    const nameFilter = (boat) =>
+      boat.id.toLowerCase().includes(NSFilter.toLowerCase());
+    const lengthFilter = (boat) =>
+      BLFilter
+        ? boat.Length >= BLFilter - 0.3 && boat.Length <= BLFilter + 0.3
+        : {};
+    const hpFilter = (boat) =>
+      BHPFilter
+        ? boat.EngineHP >= BHPFilter - 30 && boat.EngineHP <= BHPFilter + 30
+        : {};
 
-    useEffect(() => {
-        const passengerFilter = (boat) => boat.MaxPassengers >= MPFilter
-        const nameFilter = (boat) => boat.id.toLowerCase().includes(NSFilter.toLowerCase())
-        const lengthFilter = (boat) => BLFilter ? boat.Length >= BLFilter - 0.3 && boat.Length <= BLFilter + 0.3 : {}
-        const hpFilter = (boat) => BHPFilter ? boat.EngineHP >= BHPFilter - 30 && boat.EngineHP <= BHPFilter + 30 : {}
+    setBoats(
+      copy
+        .filter(passengerFilter)
+        .filter(nameFilter)
+        .filter(lengthFilter)
+        .filter(hpFilter)
+    );
+  }, [MPFilter, NSFilter, BLFilter, BHPFilter]);
 
-        setBoats(copy
-            .filter(passengerFilter)
-            .filter(nameFilter)
-            .filter(lengthFilter)
-            .filter(hpFilter)
-        )
-
-    }, [MPFilter, NSFilter, BLFilter, BHPFilter])
-
-
-    return (
-        <div>
-
-            <div className="flex flex-col" >
-                {/* {boats.map(el => (
+  return (
+    <div>
+      <div className="flex flex-col">
+        {/* {boats.map(el => (
                     <Boat
                         name={el.name}
                         price={el.price}
@@ -57,25 +61,22 @@ const Boats = ({ MPFilter, NSFilter, BLFilter, BHPFilter }) => {
                         soonestFreeDate={el.soonestFreeDate} />
                 ))} */}
 
-                {
-                    boats.map(el => (
-                        <Boat
-                            name={el.id}
-                            key={el.id}
-                            price={el.Price}
-                            maxpassengers={el.MaxPassengers}
-                            length={el.Length}
-                            width={el.Width}
-                            engineHP={el.EngineHP}
-                            fuel={el.Fuel}
-                            additional={el.Additional}
-                        />
-
-                    ))
-                }
-            </div>
-        </div>
-    )
-}
+        {boats.map((el) => (
+          <Boat
+            name={el.id}
+            key={el.id}
+            price={el.Price}
+            maxpassengers={el.MaxPassengers}
+            length={el.Length}
+            width={el.Width}
+            engineHP={el.EngineHP}
+            fuel={el.Fuel}
+            additional={el.Additional}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Boats;
